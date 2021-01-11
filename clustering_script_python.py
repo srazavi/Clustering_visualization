7825e12495a39919580ce676d9ec0fd8889fc928
Shahab Razavi <shahabrazavi01@gmail.com>
2021-01-10 21:34:05 -0500

    Added script for clustering with list of hyperparameters
    New:
    - First functional version of script to create clustering and cluster annotations with different numbers of nearest neighbors and different resolutions, respectively
    - The above hyperparameters are provided as lists, and are referenced in the keys used to save the clustering and cluster annotation

---
+++ clustering_script_python.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import anndata as ad
+import parc as pc
+
+sc.settings.verbosity = 3             # verbosity: errors (0), warnings (1), info (2), hints (3)
+results_folder = 'write'
+adata = sc.read_csv("full_dataset_biexp_aligned.csv", first_column_names = True)
+
+nn_number_list = [10] 				  # a list of numbers of nearest neighbors to consider for clustering
+resolution_list = [0.01, 0.015]		  # a list of resolutions to consider for cluster annotation
+	
+
+def main():
+	# common pre-processing steps
+	sc.pp.scale(adata)                      
+	sc.tl.pca(adata, svd_solver='auto')
+	# the next steps vary per hyper-parameter
+	for n_neighbors in nn_number_list:
+		#clustering
+		nn_key_added = str(n_neighbors)+'_nn'
+		sc.pp.neighbors(adata, method='umap',n_neighbors=n_neighbors, 
+			n_pcs=20, key_added=nn_key_added)
+		#cluster annotation
+		for res in resolution_list:
+			leiden_key_added = 'leiden_res_'+str(res).replace('.','_')+'_'+nn_key_added
+			sc.tl.leiden(adata, resolution=res, key_added=leiden_key_added, 
+				neighbors_key=nn_key_added)
+			
+	adata.write(results_folder+'/results.h5ad')
+
+if __name__== "__main__":
+	main()
+
+
