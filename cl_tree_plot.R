6ebdf3e91554c76b8e7e999fa2b5997c70abb1c5
Shahab Razavi <shahabrazavi01@gmail.com>
2021-01-18 19:31:29 -0500

    Creates directory to save plots into

--- cl_tree_plot.R
+++ cl_tree_plot.R
@@ -10,6 +10,7 @@ clustering_res_prefix <- "lr_"
 Convert("write/results.h5ad", dest = "h5seurat", overwrite = T)
 scanpy_object_Seurat <- LoadH5Seurat(paste("write/results.h5seurat", sep=""))
 
+dir.create("plots")
 for (val in nn_number_vector) {
   nn_col_suffix <- paste("_", val, nn_suffix, sep="")
   clusters <- scanpy_object_Seurat@meta.data %>% select(ends_with(nn_col_suffix))
@@ -17,5 +18,5 @@ for (val in nn_number_vector) {
   clustree_graph <- 
     clustree(clusters, prefix=clustering_res_prefix, 
              suffix=nn_col_suffix, label_nodes=T)
-  ggsave(paste("clustering_tree_", nn_col_suffix, ".png", sep=""), clustree_graph)
+  ggsave(paste("plots/clustering_tree_", nn_col_suffix, ".png", sep=""), clustree_graph)
 }
