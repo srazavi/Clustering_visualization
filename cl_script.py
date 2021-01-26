a3849c324ef4c2ecdd9507ddfe260e2481ce50c5
Shahab Razavi <shahabrazavi01@gmail.com>
2021-01-25 21:42:41 -0500

    Changed key for consistency and ease of reading

--- cl_script.py
+++ cl_script.py
@@ -4,11 +4,11 @@ import scanpy as sc
 import anndata as ad
 import parc as pc
 
-sc.settings.verbosity = 3             # verbosity: errors (0), warnings (1), info (2), hints (3)
+sc.settings.verbosity = 3             				  # verbosity: errors (0), warnings (1), info (2), hints (3)
 results_folder = 'write'
 adata = sc.read_csv("full_dataset_biexp_aligned.csv", first_column_names = True)
 
-nn_number_list = [10, 12] 				  # a list of numbers of nearest neighbors to consider for clustering
+nn_number_list = [10, 12] 				  			  # a list of numbers of nearest neighbors to consider for clustering
 resolution_list = [0.001, 0.010, 0.015, 0.02]		  # a list of resolutions to consider for cluster annotation
 	
 
@@ -20,7 +20,7 @@ def main():
 	for n_neighbors in nn_number_list:
 		#clustering
 		nn_key_added = str(n_neighbors)+'_nn'
-		umap_obsm_key = 'X_umap'+nn_key_added
+		umap_obsm_key = 'X_umap_'+nn_key_added
 		sc.pp.neighbors(adata, method='umap',n_neighbors=n_neighbors, 
 			n_pcs=20, key_added=nn_key_added)
 		adata_copy = sc.tl.umap(adata, min_dist=1, spread=1, \
