03cc967c35d4a91124cac7215cfbd1ee2b0c81c5
Shahab Razavi <shahabrazavi01@gmail.com>
2021-02-08 11:33:37 -0500

    Removed pkg-resources==0.0.0

--- convert_rdsSeurat_h5ad.R
+++
@@ -1,9 +0,0 @@
-library(Seurat)
-library(SeuratDisk)
-
-data <- readRDS(file = "org.combinedR2_clusters_names.rds")
-# data@assays$RNA@counts@Dimnames$
-
-SaveH5Seurat(data, filename = "org.combinedR2_clusters_names.h5Seurat")
-Convert("org.combinedR2_clusters_names.h5Seurat", dest = "h5ad", overwrite = T)
-
